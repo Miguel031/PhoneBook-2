7e6dc5596df7ba2987da9bb2eb125788ecb5f851
visual_community@outlook.com <visual_community@outlook.com>
2019-08-31 13:20:46 -0400

    Web api

---
+++ mvcPHONE/ApiPhone/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ApiPhone.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
